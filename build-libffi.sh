e0bdff1fa27a02bb597a6a3f1a41efdba10c9f6d
Lightech <lightech@outlook.com>
2021-02-24 19:49:50 -0800

    For some reason, must quoted strings in assignment; otherwise RHS will be interpreted as command

--- build-libffi.sh
+++ build-libffi.sh
@@ -2,26 +2,26 @@
 # Install with homebrew or from source directly
 # TODO Maybe check if the commands are present and build the ones we need? Homebrew is fairly big and time-consuming to install.
 
-PLATFORM=$1
+PLATFORM="$1"
 
 case $PLATFORM in
   "iOS")
-	SDK=iphoneos
-	SDK_ARG=-sdk $SDK;;
+    SDK="iphoneos"
+    SDK_ARG="-sdk $SDK";;
   "iOS-Sim")
     echo "Prepare LLVM for iOS simulator"
-    SDK=iphonesimulator
-	SDK_ARG=-sdk $SDK;;
+    SDK="iphonesimulator"
+    SDK_ARG="-sdk $SDK";;
   "macOS")
-    SDK=maccatalyst;;
-	# SDK_ARG=-sdk $SDK # Do not set SDK_ARG
+    SDK="maccatalyst";;
+    # SDK_ARG=-sdk $SDK # Do not set SDK_ARG
   *)
     echo "Unknown or missing platform!"
-	exit 1;;
+    exit 1;;
 esac
 
 REPO_DIR=`pwd`
-LIBFFI_DIR=$REPO_DIR/libffi
+LIBFFI_DIR="$REPO_DIR/libffi"
 
 git clone https://github.com/libffi/libffi.git
 
