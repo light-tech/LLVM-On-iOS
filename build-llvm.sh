b35e2dbff6841e05d42096e6cc0c0e2efca7f3eb
Lightech <lightech@outlook.com>
2022-10-22 03:02:45 -0400

    Rename script local variables

--- build-llvm.sh
+++ build-llvm.sh
@@ -3,71 +3,68 @@
 # The script arguments are the platforms to build
 #
 # We assume that all required build tools (CMake, ninja, etc.) are either installed and accessible in $PATH
-# or are available locally within this repo root at $REPO_ROOT/tools/bin (building on VSTS).
+# or are available locally within this repo root at $REPO_ROOT/tools/bin (building on GitHub Action).
 
-PLATFORMS_TO_BUILD=( "$@" )
+# Assume that this script is source'd at this repo root
+export REPO_ROOT=`pwd`
 
 # List of platforms-architecture that we support
 # iphoneos iphonesimulator maccatalyst
 # AVAILABLE_PLATFORMS=(iphoneos iphonesimulator-arm64 maccatalyst-arm64)
 
-# Constants
-export REPO_ROOT=`pwd`
+### Setup $targetBasePlatform, $targetArch and $libffiInstallDir from platform-architecture string
+setup_variables() {
+    local targetPlatformArch=$1
 
-### Setup $BASE_PLATFORM, $ARCH and $LIBFFI_INSTALL_DIR from platform-architecture string
-function setup_variables() {
-    local PLATFORM_ARCH=$1
-
-    case $PLATFORM_ARCH in
+    case $targetPlatformArch in
         "iphoneos")
-            ARCH="arm64"
-            BASE_PLATFORM=$PLATFORM_ARCH;;
+            targetArch="arm64"
+            targetBasePlatform="iphoneos";;
 
         "iphonesimulator")
-            ARCH="x86_64"
-            BASE_PLATFORM="iphonesimulator";;
+            targetArch="x86_64"
+            targetBasePlatform="iphonesimulator";;
 
         "iphonesimulator-arm64")
-            ARCH="arm64"
-            BASE_PLATFORM="iphonesimulator";;
+            targetArch="arm64"
+            targetBasePlatform="iphonesimulator";;
 
         "maccatalyst")
-            ARCH="x86_64"
-            BASE_PLATFORM="maccatalyst";;
+            targetArch="x86_64"
+            targetBasePlatform="maccatalyst";;
 
         "maccatalyst-arm64")
-            ARCH="arm64"
-            BASE_PLATFORM="maccatalyst";;
+            targetArch="arm64"
+            targetBasePlatform="maccatalyst";;
 
         *)
             echo "Unknown or missing platform!"
             exit 1;;
     esac
 
-    LIBFFI_INSTALL_DIR=$REPO_ROOT/libffi/Release-$BASE_PLATFORM
+    libffiInstallDir=$REPO_ROOT/libffi/Release-$targetBasePlatform
 }
 
 # Build libffi for a given platform
-function build_libffi() {
-    local PLATFORM=$1
-    setup_variables $PLATFORM
-    local LIBFFI_BUILD_DIR=$REPO_ROOT/libffi
+build_libffi() {
+    local targetPlatformArch=$1
+    setup_variables $targetPlatformArch
 
-    echo "Build libffi for $PLATFORM"
+    echo "Build libffi for $targetPlatformArch"
 
     cd $REPO_ROOT
     test -d libffi || git clone https://github.com/libffi/libffi.git
     cd libffi
 
-    case $PLATFORM in
+    case $targetPlatformArch in
         "iphoneos")
-            SDK_ARG=(-sdk $BASE_PLATFORM);;
+            xcodeSdkArgs=(-sdk $targetBasePlatform);;
 
         "iphonesimulator"|"iphonesimulator-arm64")
-            SDK_ARG=(-sdk $BASE_PLATFORM -arch $ARCH);;
+            xcodeSdkArgs=(-sdk $targetBasePlatform -arch $targetArch);;
 
         "maccatalyst"|"maccatalyst-arm64")
-            SDK_ARG=(-arch $ARCH);; # Do not set SDK_ARG
+            xcodeSdkArgs=(-arch $targetArch);; # Do not set SDK
 
         *)
             echo "Unknown or missing platform!"
@@ -77,14 +74,15 @@ function build_libffi() {
     # xcodebuild -list
     # Note that we need to run xcodebuild twice
     # The first run generates necessary headers whereas the second run actually compiles the library
+    local libffiBuildDir=$REPO_ROOT/libffi
     for r in {1..2}; do
-        xcodebuild -scheme libffi-iOS ${SDK_ARG[@]} -configuration Release SYMROOT="$LIBFFI_BUILD_DIR" >/dev/null 2>/dev/null
+        xcodebuild -scheme libffi-iOS "${xcodeSdkArgs[@]}" -configuration Release SYMROOT="$libffiBuildDir" >/dev/null 2>/dev/null
     done
 
-    lipo -info $LIBFFI_INSTALL_DIR/libffi.a
+    lipo -info $libffiInstallDir/libffi.a
 }
 
-function get_llvm_src() {
+get_llvm_src() {
     #git clone --single-branch --branch release/14.x https://github.com/llvm/llvm-project.git
 
     curl -OL https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.3/llvm-project-15.0.3.src.tar.xz
@@ -97,13 +95,14 @@ function get_llvm_src() {
 #  * ninja was extracted at this repo root
 #  * LLVM is checked out inside this repo
 #  * libffi is either built or downloaded in relative location libffi/Release-*
-function build_llvm() {
-    local PLATFORM=$1
-    local LLVM_DIR=$REPO_ROOT/llvm-project
-    local LLVM_INSTALL_DIR=$REPO_ROOT/LLVM-$PLATFORM
-    setup_variables $PLATFORM
+build_llvm() {
+    local targetPlatformArch=$1
+    local llvmProjectSrcDir=$REPO_ROOT/llvm-project
+    local llvmInstallDir=$REPO_ROOT/LLVM-$targetPlatformArch
 
-    echo "Build llvm for $PLATFORM"
+    setup_variables $targetPlatformArch
+
+    echo "Build llvm for $targetPlatformArch"
 
     cd $REPO_ROOT
     test -d llvm-project || get_llvm_src
@@ -114,7 +113,7 @@ function build_llvm() {
 
     # https://opensource.com/article/18/5/you-dont-know-bash-intro-bash-arrays
     # ;lld;libcxx;libcxxabi
-    local CMAKE_ARGS=(-G "Ninja" \
+    local llvmCmakeArgs=(-G "Ninja" \
         -DLLVM_ENABLE_PROJECTS="clang" \
         -DLLVM_TARGETS_TO_BUILD="AArch64;X86" \
         -DLLVM_BUILD_TOOLS=OFF \
@@ -128,36 +127,34 @@ function build_llvm() {
         -DLLVM_ENABLE_RTTI=OFF \
         -DLLVM_ENABLE_TERMINFO=OFF \
         -DLLVM_ENABLE_FFI=ON \
-        -DFFI_INCLUDE_DIR=$LIBFFI_INSTALL_DIR/include/ffi \
-        -DFFI_LIBRARY_DIR=$LIBFFI_INSTALL_DIR \
+        -DFFI_INCLUDE_DIR=$libffiInstallDir/include/ffi \
+        -DFFI_LIBRARY_DIR=$libffiInstallDir \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX=$LLVM_INSTALL_DIR \
+        -DCMAKE_INSTALL_PREFIX=$llvmInstallDir \
         -DCMAKE_TOOLCHAIN_FILE=../llvm/cmake/platforms/iOS.cmake)
 
-    case $PLATFORM in
+    case $targetPlatformArch in
         "iphoneos")
-            CMAKE_ARGS+=(-DLLVM_TARGET_ARCH=$ARCH);;
+            llvmCmakeArgs+=(-DLLVM_TARGET_ARCH=$targetArch);;
 
         "iphonesimulator"|"iphonesimulator-arm64")
-            SYSROOT=`xcodebuild -version -sdk iphonesimulator Path`
-            CMAKE_ARGS+=(-DCMAKE_OSX_SYSROOT=$SYSROOT);;
+            llvmCmakeArgs+=(-DCMAKE_OSX_SYSROOT=$(xcodebuild -version -sdk iphonesimulator Path));;
 
         "maccatalyst"|"maccatalyst-arm64")
-            SYSROOT=`xcodebuild -version -sdk macosx Path`
-            CMAKE_ARGS+=(-DCMAKE_OSX_SYSROOT=$SYSROOT \
-                -DCMAKE_C_FLAGS="-target $ARCH-apple-ios14.1-macabi" \
-                -DCMAKE_CXX_FLAGS="-target $ARCH-apple-ios14.1-macabi");;
+            llvmCmakeArgs+=(-DCMAKE_OSX_SYSROOT=$(xcodebuild -version -sdk macosx Path) \
+                -DCMAKE_C_FLAGS="-target $targetArch-apple-ios14.1-macabi" \
+                -DCMAKE_CXX_FLAGS="-target $targetArch-apple-ios14.1-macabi");;
 
         *)
             echo "Unknown or missing platform!"
             exit 1;;
     esac
 
-    CMAKE_ARGS+=(-DCMAKE_OSX_ARCHITECTURES=$ARCH)
+    llvmCmakeArgs+=(-DCMAKE_OSX_ARCHITECTURES=$targetArch)
 
     # https://www.shell-tips.com/bash/arrays/
     # https://www.lukeshu.com/blog/bash-arrays.html
-    printf 'CMake Argument: %s\n' "${CMAKE_ARGS[@]}"
+    printf 'CMake Argument: %s\n' "${llvmCmakeArgs[@]}"
 
     # Generate configuration for building for iOS Target (on MacOS Host)
     # Note: AArch64 = arm64
@@ -165,7 +162,7 @@ function build_llvm() {
     # includes <ffi_arm64.h> and not <ffi/ffi_arm64.h>. So if we only use
     # $DOWNLOADS/libffi/Release-iphoneos/include for FFI_INCLUDE_DIR
     # the platform-specific header would not be found!
-    cmake "${CMAKE_ARGS[@]}" ../llvm || exit -1 # >/dev/null 2>/dev/null
+    cmake "${llvmCmakeArgs[@]}" ../llvm || exit -1 # >/dev/null 2>/dev/null
 
     # When building for real iOS device, we need to open `build_ios/CMakeCache.txt` at this point, search for and FORCIBLY change the value of **HAVE_FFI_CALL** to **1**.
     # For some reason, CMake did not manage to determine that `ffi_call` was available even though it really is the case.
@@ -177,11 +174,11 @@ function build_llvm() {
 }
 
 # Prepare the LLVM built for usage in Xcode
-function prepare_llvm() {
-    local PLATFORM=$1
+prepare_llvm() {
+    local targetPlatformArch=$1
 
-    echo "Prepare LLVM for $PLATFORM"
-    cd $REPO_ROOT/LLVM-$PLATFORM
+    echo "Prepare LLVM for $targetPlatformArch"
+    cd $REPO_ROOT/LLVM-$targetPlatformArch
 
     # Remove unnecessary executables and support files
     rm -rf bin libexec share
@@ -194,8 +191,8 @@ function prepare_llvm() {
     rm -rf lib2 # Comment this if you want to keep
 
     # Copy libffi
-    cp -r $LIBFFI_INSTALL_DIR/include/ffi ./include/
-    cp $LIBFFI_INSTALL_DIR/libffi.a ./lib/
+    cp -r $libffiInstallDir/include/ffi ./include/
+    cp $libffiInstallDir/libffi.a ./lib/
 
     # Combine all *.a into a single llvm.a for ease of use
     libtool -static -o llvm.a lib/*.a
@@ -204,6 +201,7 @@ function prepare_llvm() {
     rm -rf lib/*.a
 }
 
+PLATFORMS_TO_BUILD=( "$@" )
 for p in ${PLATFORMS_TO_BUILD[@]}; do
     echo "Build LLVM library for $p"
 
